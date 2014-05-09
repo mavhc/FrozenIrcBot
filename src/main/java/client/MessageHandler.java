ea3ab24043ac2bcf05f17961c162cca08a4f7668
Philipp Koschinski <philipp.koschinski@googlemail.com>
2014-05-09 17:45:26 +0200

    Modified classpath

--- src/main/java/client/MessageHandler.java
+++
@@ -1,7 +0,0 @@
-package client;
-
-import packets.MessagePacket;
-
-public interface MessageHandler {
-	public boolean handleMessage(MessagePacket msg);
-}
--- src/main/java/net/PermLevel.java
+++
@@ -1,5 +0,0 @@
-package net;
-
-public enum PermLevel {
-	USER, VOICE, OPERATOR;
-}
